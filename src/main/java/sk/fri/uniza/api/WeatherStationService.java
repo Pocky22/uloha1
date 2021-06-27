ba3439544e272bb4ab3ea47479e38339422743bc
Pocky22 <pocklan@stud.uniza.sk>
2021-06-27 17:45:21 +0200

    metody pre auth

--- src/main/java/sk/fri/uniza/api/WeatherStationService.java
+++ src/main/java/sk/fri/uniza/api/WeatherStationService.java
@@ -2,9 +2,12 @@ package sk.fri.uniza.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.POST;
 import sk.fri.uniza.model.Location;
+import sk.fri.uniza.model.Token;
 import sk.fri.uniza.model.WeatherData;
 
 import java.util.List;
@@ -45,21 +48,39 @@ public interface WeatherStationService {
                                                   @Query("to") String to,
                                                   @Query("fields") List<String> fields);
 
+    @POST("/apikey/createJWT")
+    Call<Token> getToken(@Header("authorization") String authorization,
+                         @Query("claims") List<String> claims);
     // ... getToken(authorization, claims);
 
-
+    @GET("/weather/locations")
+    Call<List<Location>> getStationLocationsAuth(@Header("authorization") String authorization);
     // ... getStationLocationsAuth(authorization);
 
-
+    Call<WeatherData> getCurrentWeatherAuth(@Header("Authorization") String authorization,
+                                            @Path("station") String station);
     // ... getCurrentWeatherAuth(authorization, station);
 
-
+    @GET("/weather/{station}/current")
+    Call<WeatherData> getCurrentWeatherAuth(@Header("Authorization") String authorization ,
+                                            @Path("station") String station,
+                                            @Query("fields") List<String> fields);
     // ... getCurrentWeatherAuth(authorization, station, fields);
 
-
+    @GET("/weather/{station}/history")
+    Call<List<WeatherData>> getHistoryWeatherAuth(@Header("Authorization") String authorization,
+                                                  @Path("station") String station,
+                                                  @Query("from") String from,
+                                                  @Query("to") String to );
     // ... getHistoryWeatherAuth(authorization, station, from, to);
 
-
+    @GET("/weather/{station}/history")
+    Call<List<WeatherData>> getHistoryWeatherAuth(@Header("Authorization") String authorization,
+                                                  @Path("station") String station,
+                                                  @Query("from") String from,
+                                                  @Query("to") String to,
+                                                  @Query("fields") List<String> fields
+    );
     // ... getHistoryWeatherAuth(authorization, station, from, to, fields);
 
 }
